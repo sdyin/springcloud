6edab3ed72cdb2fbd3f2445c906348f8ae51c9e6
liuye <liuye@iboxpay.com>
2018-06-27 21:52:45 +0800

    新增 auth-service 服务(鉴权待完善)

---
+++ auth-service/src/main/java/com/sdyin/authservice/config/AuthorizationServerConfiguration.java
@@ -0,0 +1,64 @@
+package com.sdyin.authservice.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
+
+/**
+ * @Author: liuye
+ * @Date: 2018/6/27 19:48
+ */
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfiguration extends AuthorizationServerConfigurerAdapter {
+
+  private static final String DEMO_RESOURCE_ID = "order";
+
+  private static final int ACCESS_TOKEN_TIMER = 60 * 60 * 24;
+  private static final int REFRESH_TOKEN_TIMER = 60 * 60 * 24 * 30;
+
+  @Autowired
+  AuthenticationManager authenticationManager;
+  @Autowired
+  RedisConnectionFactory redisConnectionFactory;
+
+  @Override
+  public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+    //配置两个客户端,一个用于password认证一个用于client认证
+    clients.inMemory().withClient("client_1")
+            .resourceIds(DEMO_RESOURCE_ID)
+            .authorizedGrantTypes("client_credentials", "refresh_token")
+            .scopes("select")
+            .authorities("client")
+            .secret("123456")
+            .and().withClient("client_2")
+            .resourceIds(DEMO_RESOURCE_ID)
+            .authorizedGrantTypes("password", "refresh_token")
+            .scopes("select")
+            .authorities("client")
+            .secret("123456")
+            .accessTokenValiditySeconds(ACCESS_TOKEN_TIMER)
+            .refreshTokenValiditySeconds(REFRESH_TOKEN_TIMER);
+  }
+
+  @Override
+  public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+    endpoints
+            .tokenStore(new RedisTokenStore(redisConnectionFactory))
+            .authenticationManager(authenticationManager);
+  }
+
+  @Override
+  public void configure(AuthorizationServerSecurityConfigurer oauthServer) throws Exception {
+    //允许表单认证
+    oauthServer.allowFormAuthenticationForClients();
+  }
+
+}
